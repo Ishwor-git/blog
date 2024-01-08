474a67cc217650a4eeb47d33302ab48b7a9714d5
Ishwor-git <raishwor2@gmail.com>
2024-01-08 21:39:53 +0545

    added blog data

---
+++ data/blogData.js
@@ -0,0 +1,24 @@
+{
+  blogs: [
+    {
+      title: "Esse enim esse occaecat nisi aliquip qui irure ththsht.",
+      body: "Tempor deserunt magna excepteur irure consequat adipisicing sint. Sint cillum culpa nulla magna adipisicing. Eu amet occaecat do amet cupidatat deserunt sint cupidatat in nulla aliqua cupidatat dolor.",
+      slug: "blog-data-erss",
+    },
+    {
+      title: "Esse enim esse occaecat nisi aliquip qui irure yjukukf.",
+      body: "Tempor deserunt magna excepteur irure consequat adipisicing sint. Sint cillum culpa nulla magna adipisicing. Eu amet occaecat do amet cupidatat deserunt sint cupidatat in nulla aliqua cupidatat dolor.",
+      slug: "blog-data-dderf",
+    },
+    {
+      title: "Esse enim esse occaecat nisi aliquip qui irure rtgrtgg.",
+      body: "Tempor deserunt magna excepteur irure consequat adipisicing sint. Sint cillum culpa nulla magna adipisicing. Eu amet occaecat do amet cupidatat deserunt sint cupidatat in nulla aliqua cupidatat dolor.",
+      slug: "blog-data-drgra",
+    },
+    {
+      title: "Esse enim esse occaecat nisi aliquip qui irure vnynhhn.",
+      body: "Tempor deserunt magna excepteur irure consequat adipisicing sint. Sint cillum culpa nulla magna adipisicing. Eu amet occaecat do amet cupidatat deserunt sint cupidatat in nulla aliqua cupidatat dolor.",
+      slug: "blog-data-nnhys",
+    },
+  ];
+}
