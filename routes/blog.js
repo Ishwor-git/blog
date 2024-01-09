4be3f98ea2a0bbf94471cbd650683dda405eb6f7
Ishwor-git <raishwor2@gmail.com>
2024-01-09 10:09:15 +0545

    updated blog rooute do log blogs

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const router = require(express.Router);
+const router = express.Router();
+const path = require("path");
+const blog = require("../data/blogData.js");
 
 router.get("/", (req, res) => {
-  res.sendFile("public/index.html");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+router.get("/blogs", (req, res) => {
+  blog.forEach((e) => {
+    console.log(e.title);
+  });
+});
+
+module.exports = router;
