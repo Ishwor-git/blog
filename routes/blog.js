cfd0df0c04cf2a8d629164f058e293d25001453a
Ishwor-git <raishwor2@gmail.com>
2024-01-09 18:37:02 +0545

    updated routes to send blog template html files

--- routes/blog.js
+++ routes/blog.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const blog = require("../data/blogData.js");
+const blogs = require("../data/blogData.js");
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/blogs", (req, res) => {
-  blog.forEach((e) => {
-    console.log(e.title);
+  // blog.forEach((e) => {
+  //   console.log(e.title);
+  // });
+  res.status(200).sendFile(path.join(__dirname, "../templates/blogHome.html"));
+});
+
+router.get("/blogpost/:slug", (req, res) => {
+  myBlog = blogs.filter((e) => {
+    if (e.slug == req.params.slug) {
+      return e;
+    }
+    // return (e.slug = req.params.slug);
   });
+  console.log(myBlog);
+  res.status(200).sendFile(path.join(__dirname, "../templates/blogPage.html"));
 });
 
 module.exports = router;
