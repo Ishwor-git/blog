dab2c48c824f8c67f9aad81b9c68809bcddcc1a4
Ishwor-git <raishwor2@gmail.com>
2024-07-31 23:30:45 +0545

    deleting current apis and creating space for new

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-var exphbs = require("express-handlebars");
 const port = 3000;
-
-// var exphbs = require('express-handlebars');
-// app.engine(".hbs", exphbs.engine({ extname: ".hbs", defaultLayout: "main" }));
-// app.set("view engine", ".hbs");
-
-app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars");
 
 app.use(express.static(path.join(__dirname, "../static")));
 app.use("/", require(path.join(__dirname, "routes/blog.js")));
--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -3,25 +3,4 @@ const router = express.Router();
 const path = require("path");
 const blogs = require("../data/blogData.js");
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
-
-router.get("/blogs", (req, res) => {
-  res.render("blogHome", {
-    blogs: blogs,
-  });
-});
-
-router.get("/blogpost/:slug", (req, res) => {
-  myBlog = blogs.filter((e) => {
-    if (e.slug == req.params.slug) {
-      return e;
-    }
-  });
-  res.status(200).render("blogPost", {
-    blog: myBlog[0],
-  });
-});
-
 module.exports = router;
