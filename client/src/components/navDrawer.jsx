718e7c95678b49e2d9b697bd9c4a449fe15f29ff
Ishwor-git <raishwor2@gmail.com>
2024-08-02 20:33:00 +0545

    navDrawer initilizing

--- client/src/components/navDrawer.jsx
+++ client/src/components/navDrawer.jsx
@@ -7,20 +7,45 @@ import {
   ListItemText,
   Divider,
   Box,
+  Typography,
 } from "@mui/material";
+import BookOutlinedIcon from "@mui/icons-material/BookOutlined";
 import React from "react";
-
-const NavDrawer = ({ navList }) => {
+const NavDrawer = ({ navList = ["Tab1", "Tab2"] }) => {
   return (
     <>
       <Drawer open={1}>
-        <Box sx={{ minWidth: "15rem" }}>
+        <Box
+          sx={{
+            width: 250,
+          }}
+          role="presentation"
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignSelf: "center",
+              my: "1rem",
+              ml: "1.7rem",
+            }}
+          >
+            <Typography sx={{ mx: "0.2rem" }}>
+              <BookOutlinedIcon />
+            </Typography>
+            <Typography variant="h5" component="div" sx={{}}>
+              B1tB0tByte
+            </Typography>
+          </Box>
           <List>
-            <ListItem>
-              <ListItemButton>
-                <ListItemText>Hello</ListItemText>
-              </ListItemButton>
-            </ListItem>
+            {navList.map((text) => {
+              return (
+                <ListItem key={text}>
+                  <ListItemButton>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
           </List>
         </Box>
       </Drawer>
