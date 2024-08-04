ed211ad2719bed0cdf40ece7bf3afb3b0d8adc0e
Ishwor-git <raishwor2@gmail.com>
2024-08-04 22:25:29 +0545

    setting up  react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import Navbar from "./components/navbar";
-// import NavDrawer from "./components/navDrawer";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Blogs from "./pages/blogs";
+import Post from "./pages/post";
+import Contact from "./pages/contact";
+import About from "./pages/about";
 function App() {
+  const router = createBrowserRouter([
+    { path: "", element: <Home /> },
+    { path: "blogs", element: <Blogs /> },
+    { path: "post", element: <Post /> },
+    { path: "contact-us", element: <Contact /> },
+    { path: "about-us", element: <About /> },
+  ]);
   return (
     <>
       <Navbar />
+      <RouterProvider router={router} />
     </>
   );
 }
