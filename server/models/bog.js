d4eee9c88f77954653b9dd734d540c1c45ab1a39
Ishwor-git <raishwor2@gmail.com>
2024-08-07 00:18:22 +0545

    wrote schema

---
+++ server/models/bog.js
@@ -0,0 +1,32 @@
+const mongoose = require("./index.js");
+const BlogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    catagory: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      required: false,
+    },
+    picture: {
+      type: String,
+      required: false,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const Blog = mongoose.model("Blog", BlogSchema);
+module.exports = Blog;
