dcaf95bccd5e72613897737b427724b922adf540
Ishwor-git <raishwor2@gmail.com>
2024-08-08 23:32:58 +0545

    generating admin and jwt

--- server/app.js
+++ server/app.js
@@ -4,9 +4,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 6000;
 const mongoose = require("./models");
+// const { createAdmin } = require("./generator/admin");
 app.use(express.json());
 app.use("/", require(path.join(__dirname, "routes/routes.js")));
 
 app.listen(port, (req, res) => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+//check if mongoose is connected and run createAdmin function this is to be run only once
+// mongoose.connection.once("open", () => {
+//   createAdmin();
+// });
