4b64a368bf8b24830b5d8fb41fcaed2a3cee5e6e
Ishwor-git <raishwor2@gmail.com>
2024-08-09 14:58:30 +0545

    email api setup using mailgun

--- server/routes/api/contact.js
+++ server/routes/api/contact.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const nodeMailer = require("nodemailer");
+// const nodeMailer = require("nodemailer");
+const mg = require("mailgun-js");
 
 router.get("/test", (req, res) => {
   res.send({ data: "testing contact" });
@@ -9,32 +10,31 @@ router.get("/test", (req, res) => {
 
 // router.get("/testCredentials", (req, res) => {
 //   res.json({
-//     username: process.env.ETHEREAL_USER,
-//     password: process.env.ETHEREAL_PASSWORD,
+//     username: process.env.EMAIL_USER,
+//     password: process.env.EMAIL_PASSWORD,
 //     contact_email: process.env.CONTACT_EMAIL,
 //   });
 // });
 router.post("/", async (req, res) => {
-  const { name, email, message } = req.body;
-  if (!name || !email || !message) {
+  const { name, email, subject, message } = req.body;
+  if (!name || !email || !subject || !message) {
     return res.status(400).json({ error: "All fields are required" });
   }
+
   try {
-    const transporter = nodeMailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      auth: {
-        user: process.env.ETHEREAL_USER,
-        pass: process.env.ETHEREAL_PASSWORD,
-      },
-    });
+    const mailgun = () =>
+      mg({
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+      });
     const mailOptions = {
       from: email,
       to: process.env.CONTACT_EMAIL,
-      subject: `Message from ${name}`,
-      text: message,
+      subject: `Message through bitbotbyte blog from ${name}`,
+      text: `Subject : ${subject} \n\n ${message}`,
     };
-    await transporter.sendMail(mailOptions);
+    // console.log(mailOptions);
+    await mailgun().messages().send(mailOptions);
     res.status(200).json({ message: "Email sent successfully" });
   } catch (err) {
     console.error("Error  sending emaiil : ", err);
