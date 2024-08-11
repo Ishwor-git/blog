bac52884379d32e7fa5e8c3c85ff22c5c6d87fba
Ishwor-git <raishwor2@gmail.com>
2024-08-11 14:37:43 +0545

    authprovider context

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./main";
+import AuthProvider from "./context/authProvider";
 function App() {
   return (
-    <Router>
-      <Main />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Main />
+      </Router>
+    </AuthProvider>
   );
 }
 
